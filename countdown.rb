50edc7b31c08d4324283d24c4c1d50ca761d96ca
Lauren Giordano <laurengiordano94@gmail.com>
2023-05-04 13:26:49 +1000

    done with lab

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown num
+    x = num
+    while x > 0 
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+countdown 10
+
+
+def countdown_with_sleep num
+    x = num
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+        sleep 1
+    end
+    "HAPYY NEW YEAR!"
+end
+
+countdown_with_sleep 10
